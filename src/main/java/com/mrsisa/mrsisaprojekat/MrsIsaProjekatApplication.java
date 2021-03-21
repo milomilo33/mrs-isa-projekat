28c282dd9caf4862eb22dd1fdae46ac4bbd6ce5b
milomilo33 <milovanovicm309@gmail.com>
2021-03-21 15:36:33 +0100

    Kreiran inicijalni (Hello World) projekat sa Spring Boot backend-om i Vue.js frontend-om.

---
+++ src/main/java/com/mrsisa/mrsisaprojekat/MrsIsaProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.mrsisa.mrsisaprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrsIsaProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrsIsaProjekatApplication.class, args);
+	}
+
+}
