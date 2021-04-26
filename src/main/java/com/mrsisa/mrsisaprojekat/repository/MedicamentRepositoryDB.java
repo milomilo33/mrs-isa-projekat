9facd4bff927a9b664a9d66060ebb5f60d709d38
draganaF <filipovic.dada@gmail.com>
2021-04-26 22:43:23 +0200

    kreirana pretraga i filtriranje

--- src/main/java/com/mrsisa/mrsisaprojekat/repository/MedicamentRepositoryDB.java
+++ src/main/java/com/mrsisa/mrsisaprojekat/repository/MedicamentRepositoryDB.java
@@ -1,5 +1,7 @@
 package com.mrsisa.mrsisaprojekat.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +10,10 @@ import com.mrsisa.mrsisaprojekat.model.Medicament;
 public interface MedicamentRepositoryDB extends JpaRepository<Medicament, Long>{
 	@Query("select p from Medicament p join fetch p.ratings where p.id=?1 and p.deleted=false")
 	Medicament getRatings(Long id);
+
+	@Query("select m from Medicament m where lower(m.name) like lower(concat('%', concat(?1, '%')))")
+	Collection<Medicament> findAllWithName(String query);
+
+	@Query("select m from Medicament m where m.issuanceMode=?1 or m.medicamentForm=?2")
+	Collection<Medicament> findFilter(int mode, int form);
 }
