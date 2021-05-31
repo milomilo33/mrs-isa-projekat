f7db7e2221e99bd00ae3634f9f20ae1014b7e339
Nikolina Tosic <nikolinatosic999@gmail.com>
2021-05-31 21:21:27 +0200

    Dodato brisanje penala svakog 1. u mesecu kao i provera da li je pacijentu dozvoljeno zakazivanje

--- src/main/java/com/mrsisa/mrsisaprojekat/MrsIsaProjekatApplication.java
+++ src/main/java/com/mrsisa/mrsisaprojekat/MrsIsaProjekatApplication.java
@@ -3,9 +3,11 @@ package com.mrsisa.mrsisaprojekat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class MrsIsaProjekatApplication {
 
 //	@Bean
