bd4b10237661e75133c481e5d84c03427d8f13bf
milomilo33 <milovanovicm309@gmail.com>
2021-06-15 04:04:20 +0200

    Heroku

--- src/main/ui/vue/server.js
+++ src/main/ui/vue/server.js
@@ -1,13 +1,22 @@
 // server.js
 
 var express = require('express');
+var history = require('connect-history-api-fallback')
 var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
+
+// Use a fallback for non-root routes (required for Vue router)
+// NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
 app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 8081;
 app.listen(port);
 
-console.log('server started '+ port);
+console.log('server started ' + port);
