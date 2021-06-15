6f464fc93019e3075729fee662a8e5511ed4ecb3
milomilo33 <milovanovicm309@gmail.com>
2021-06-15 06:20:46 +0200

    Heroku

--- src/main/ui/vue/server.js
+++ src/main/ui/vue/server.js
@@ -10,14 +10,34 @@ app = express();
 
 app.use(cors());
 
+// app.use(serveStatic(path.join(__dirname, '/dist')))
+
+// app.get('/style.css', function(req, res){
+//     res.sendfile(__dirname + '/dist/css/style.css');
+// });
+
+app.use(express.static('dist'));
+
 // Use a fallback for non-root routes (required for Vue router)
 // NOTE: History fallback must be "used" before the static serving middleware!
 app.use(history({
     // OPTIONAL: Includes more verbose logging
-    verbose: true
+    verbose: true,
+    htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
 }))
 
-app.use(serveStatic(__dirname + "/dist/"));
+// app.use(express.static(__dirname + "/dist"));
+
+// // app.get(/.*/, (req, res) => {
+// //     res.sendFile(__dirname + '/dist/index.html');
+// // });
+
+app.use(express.static('dist'));
+
+// // this * route is to serve project on different page routes except root `/`
+// app.get(/.*/, function (req, res) {
+// 	res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
 
 var port = process.env.PORT || 8081;
 app.listen(port);
--- src/main/ui/vue/vue.config.js
+++ src/main/ui/vue/vue.config.js
@@ -6,5 +6,9 @@ module.exports = {
           changeOrigin: true
         },
       }
+    },
+    publicPath: '/',
+    css: {
+      extract: true
     }
   }
